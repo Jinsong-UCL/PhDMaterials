7efefe997120faac4cc12bc625925970acd9d584
Jinsong Hao <73044933+Jinsong-UCL@users.noreply.github.com>
2023-04-24 14:26:07 +0100

    Refactored

--- Matlab/AsymmetricMatrices/HCF.m
+++ Matlab/AsymmetricMatrices/HCF.m
@@ -1,4 +1,4 @@
-function [option_price] = HCF(market,param,theta)
+function [option_price] = HCF(market,param,fourier,theta)
 %% Retrieve parameters 
 S0 = market.S0;
 K = market.K;
@@ -6,35 +6,26 @@ d = market.d;
 T = market.T;
 
 beta = param.beta;
-am = param.am;
-an = param.an;
-hm = param.hm;
-hn = param.hn;
-y_0 = param.y_0;
+An = param.An;
+Am = param.Am;
+Rn = param.Rn;
+Rm = param.Rm;
+R = Rn - Rm;
+V_0 = param.V_0;
 
 rho = param.rho;
 kappa = param.kappa;
 sigma = param.sigma;
 
-h = hm - hn;
-
-% Damping parameter
-alpha = -4*theta;
-
-% Fourier parameters
-xwidth = 20; % width of the support in real space
-ngrid = 2^10; % number of grid points
-
-% Grids in real and Fourier space
-N = ngrid/2;
-B = xwidth/2; % upper bound of the support in real space
-dxi = pi/B; % Nyquist relation: grid step in Fourier space
-xi = dxi*(-N:N-1); % grid in Fourier space
-xi_shifted = xi +1i*alpha;
+ngrid = fourier.ngrid; % number of grid points
+dxi = fourier.dxi; 
+xi = fourier.xi; 
 
 % Auxiliary parameters
-a_minus = am - an;
-a_plus = am + an; 
+alpha = -2*theta; % Damping parameter
+xi_shifted = xi +1i*alpha;
+a_minus = An - Am;
+a_plus = An + Am; 
 
 CF = zeros(1,ngrid);
 for i = 1:ngrid
@@ -44,9 +35,9 @@ for i = 1:ngrid
     e2 = kappa'- a_minus*rho'*sigma*1i*x;
     E = 0.5*(e1+e2);
     F = a_minus*a_minus*x^2 - a_plus*a_minus*1i*x;
-    D = sqrtm(E*E + sigma'*sigma* (F - 2 * h*1i*x + 2*hm));
+    D = sqrtm(E*E + 2*sigma'*sigma* (F - 2 * R*1i*x + 2*Rn));
     G = (E - D)/(E + D);    
-    CF(i) = trace(beta*((E-D)*T-2*logm((eye(d)-G*expm(-D*T))/(eye(d)-G)))) + trace(y_0*eye(d)/(sigma'*sigma)*((E-D)*(eye(d)-expm(-D*T))/(eye(d)-G*expm(-D*T))));
+    CF(i) = trace(0.5*beta*((E-D)*T-2*logm((eye(d)-G*expm(-D*T))/(eye(d)-G)))) + trace(V_0*eye(d)/(2*sigma'*sigma)*((E-D)*(eye(d)-expm(-D*T))/(eye(d)-G*expm(-D*T))));
 end
 CF_E = exp(CF);
 factor_simple = S0;
--- Matlab/AsymmetricMatrices/RunGG.m
+++ Matlab/AsymmetricMatrices/RunGG.m
@@ -4,30 +4,31 @@ warnStruct = warning('off',warnId);
 
 %% Market parameters 
 marketStruct.S0 = 1;
-marketStruct.K = 1;
+marketStruct.K = 0.9;
 marketStruct.d = 2;
 marketStruct.T = 1;
 % The following numbers are from Gnoatto and Grasselli 2014
-%% Diagonal parameters
-% paramStruct.beta = 3.1442;
-% paramStruct.am = [0.7764 0.0;0.0 0.9639];
-% paramStruct.an = [0.6679 0.0;0.0 0.8520];
-% paramStruct.hm = [0.2725 0.0;0.0 0.4726];
-% paramStruct.hn = [0.1841 0.0;0.0 0.4700];
-% paramStruct.y_0 = [0.1688 0.0;0.0 0.3169];
-% paramStruct.rho = [-0.5417 0.0;0.0 -0.4834];
-% paramStruct.kappa = [1.0426,0.0;0.0,0.8778]; 
-% paramStruct.sigma = [0.4364,0.0;0.0,0.7362];
 %% Symmetric parameters
 paramStruct.beta = 3.144;
-paramStruct.am = [0.7764 0.4837;0.4837 0.9639];
-paramStruct.an = [0.6679 0.6277;0.6277 0.8520];
-paramStruct.hm = [0.2725 0.0804;0.0804 0.4726];
-paramStruct.hn = [0.1841 0.0155;0.0155 0.4761];
-paramStruct.y_0 = [0.1688 0.1708;0.1708 0.3169];
+paramStruct.An = [0.7764 0.4837;0.4837 0.9639];
+paramStruct.Am = [0.6679 0.6277;0.6277 0.8520];
+paramStruct.Rn = [0.2725 0.0804;0.0804 0.4726];
+paramStruct.Rm = [0.1841 0.0155;0.0155 0.4761];
+paramStruct.V_0 = [0.1688 0.1708;0.1708 0.3169];
 paramStruct.rho = [-0.5417 0.1899;-0.1170 -0.4834];
 paramStruct.kappa = [1.0426,0.6764;0.9880,0.8778]; 
-paramStruct.sigma = [0.4368,0.1999;0.1999,0.7362];
+paramStruct.sigma = [0.4368,0.2014;0.3514,0.7362];
+
+%% Fourier parameters
+fourierStruct.xwidth = 20; % width of the support in real space
+fourierStruct.ngrid = 2^12; % number of grid points
+
+% Grids in real and Fourier space
+fourierStruct.N = fourierStruct.ngrid/2;
+fourierStruct.B = fourierStruct.xwidth/2; % upper bound of the support in real space
+fourierStruct.dxi = pi/fourierStruct.B; % Nyquist relation: grid step in Fourier space
+fourierStruct.xi = fourierStruct.dxi*(-fourierStruct.N:fourierStruct.N-1); % grid in Fourier space
+
 % rho test this is to test if 
 if sum(eig(eye(marketStruct.d)-paramStruct.rho*paramStruct.rho.')>=0) == marketStruct.d 
     fprintf("rho is valid, please continue.\n");
@@ -36,18 +37,18 @@ else
 end
 
 %% Simulation without constant dispalcements in the interets rates
-[simulated_call, simulated_put, phi_empirical] = GGsimulation(marketStruct,paramStruct);
+[simulated_call, simulated_put, CF_E] = GGsimulation(marketStruct,paramStruct,fourierStruct);
 
 %% CF
 fprintf("This is the result of CF in our format\n")
-[call_price_H] = HCF(marketStruct,paramStruct,1);
-[put_price_H] = HCF(marketStruct,paramStruct,-1);
+[call_price_H] = HCF(marketStruct,paramStruct,fourierStruct,1);
+[put_price_H] = HCF(marketStruct,paramStruct,fourierStruct,-1);
 
 %% CFG
 fprintf("This is the result of CF in Da Fonseca's format\n")
-[call_price] = GCF(marketStruct,paramStruct,1);
-[put_price] = GCF(marketStruct,paramStruct,-1);
+[call_price] = GCF(marketStruct,paramStruct,fourierStruct,1);
+[put_price] = GCF(marketStruct,paramStruct,fourierStruct,-1);
 
 %% ECF and ACF
-[statement] = ECFACF(marketStruct,paramStruct,phi_empirical);
+[statement] = ECFACF(marketStruct,paramStruct,fourierStruct,CF_E);
 
